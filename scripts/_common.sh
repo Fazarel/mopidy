e76d8119997061e685132996fb7e81b738983879
Azarel Faralca <framagit.moq6f@simplelogin.co>
2023-10-29 16:42:45 +0100

    modified: scripts/install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,7 +11,7 @@
 # Add Mopidy APT archive
 add_mopidy_repo () {
     mkdir -p /etc/apt/keyrings
-    wget -q -O /etc/apt/keyrings/mopidy-archive-keyring.gpg https://apt.mopidy.com/mopidy.gpg
+    wget -q -O /etc/apt/keyrings/mopidy-archive-keyring.gpg \ https://apt.mopidy.com/mopidy.gpg
     wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/bullseye.list
 }
 
