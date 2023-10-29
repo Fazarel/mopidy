d70975a525b2d4fc0522ed5736e8125135b53e20
Azarel Faralca <framagit.moq6f@simplelogin.co>
2023-10-29 17:30:40 +0100

    Trying extra repo method in manifest

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,17 +9,17 @@
 #=================================================
 
 # Add Mopidy APT archive
-add_mopidy_repo () {
-    sudo mkdir -p /etc/apt/keyrings
-    sudo wget -q -O /etc/apt/keyrings/mopidy-archive-keyring.gpg https://apt.mopidy.com/mopidy.gpg
-    sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/bullseye.list
-}
+#add_mopidy_repo () {
+#    sudo mkdir -p /etc/apt/keyrings
+#    sudo wget -q -O /etc/apt/keyrings/mopidy-archive-keyring.gpg https://apt.mopidy.com/mopidy.gpg
+#    sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/bullseye.list
+#}
 
 # Remoce Mopidy APT archive
-remove_mopidy_repo () {
-    sudp rm -r /etc/apt/keyrings
-    sudo rm /etc/apt/sources.list.d/mopidy.list
-}
+#remove_mopidy_repo () {
+#    sudo rm -r /etc/apt/keyrings
+#    sudo rm /etc/apt/sources.list.d/mopidy.list
+#}
 
 #=================================================
 # EXPERIMENTAL HELPERS
